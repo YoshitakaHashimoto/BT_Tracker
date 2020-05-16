10e8a55a4423a65f589305cc4237eab4c6e2f706
YoshitakaHashimoto <2020m074@kuas.ac.jp>
2020-05-16 13:42:29 +0900

    Added Normal Activity and intent to normal

--- app/src/main/java/com/example/bt_tracker/MainActivity.java
+++ app/src/main/java/com/example/bt_tracker/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent toLog = new Intent(this,LogActivity.class);
         startActivity(toLog);
     }
+
+    public void gotoNormalActivity(View view) {
+        Intent toNormal = new Intent(this,NormalActivity.class);
+        startActivity(toNormal);
+    }
 }
