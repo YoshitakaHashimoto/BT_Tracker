12201cd7a263928705d2e028efa6f53f60d7f3c4
YoshitakaHashimoto <2020m074@kuas.ac.jp>
2020-06-05 20:18:35 +0900

    error in Notification

--- app/src/main/java/com/example/bt_tracker/MainActivity.java
+++ app/src/main/java/com/example/bt_tracker/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.bt_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.security.cert.Extension;
 import java.time.ZoneOffset;
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
 
         TextView mainButton1 = findViewById(R.id.MainButton1);
         // Add "an ear" to MainButton1
@@ -61,6 +68,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     startActivity(toLink1);
                 }
                 break;
+        }
+    }
+
+    public void SetReminder(View view) {
+        // When user clicks "SET REMINDER" button, a toast message will pop up to let user know that are reminder is set
+        Toast.makeText(this, "Reminder set!", Toast.LENGTH_SHORT).show();
+        // Create an intent object to start the ReminderBroadcastReceiver Class
+        Intent intent = new Intent(this, ReminderBroadcastReceiver.class);
+        // Create a pending intent so that the intent object above will only fire when alarm triggers
+        PendingIntent pd = PendingIntent.getBroadcast(this,0,intent, 0);
+        // create an AlarmManager
+        AlarmManager alarmManager= (AlarmManager)getSystemService(ALARM_SERVICE);
+        // Repeating interval for the alarmManager is set to 6 second for demonstration purpose
+        // In real world application, users may want to get daily reminder
+        // In that case, set the interval to 1000 * 60 * 60 * 24
+        long interval = 1000*6;
+        // set up a repeating alarm so that the notification reminder gets fired at the set interval
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                          System.currentTimeMillis(), interval,pd);
+
+    }
+
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
+            String channelID = "BT_Tracker_Channel";
+            String channelName = "BTTrackerReminderChannel";
+            String channelDescription = "Channel for BT Tracker reminder";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelID,channelName, importance);
+            channel.setDescription(channelDescription);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
