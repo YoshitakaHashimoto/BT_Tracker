64c2dd59fc4b2661216f2854ab970394f7092a27
YoshitakaHashimoto <2020m074@kuas.ac.jp>
2020-07-03 11:43:30 +0900

    Go to link in  MechanismActivity

--- app/src/main/java/com/example/bt_tracker/MechanismActivity.java
+++ app/src/main/java/com/example/bt_tracker/MechanismActivity.java
@@ -3,6 +3,7 @@ package com.example.bt_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,5 +18,11 @@ public class MechanismActivity extends AppCompatActivity {
     public void backToHome(View view) {
         Intent backHome = new Intent(this, MainActivity.class);
         startActivity(backHome);
+    }
+
+    public void goToLink1(View view) {
+        Intent toLink1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scientificamerican.com/article/what-causes-a-fever/"));
+
+        startActivity(toLink1);
     }
 }
