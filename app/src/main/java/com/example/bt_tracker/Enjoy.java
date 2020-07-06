de4bc923ff8a5ba4b072ce514851663153706771
YoshitakaHashimoto <2020m074@kuas.ac.jp>
2020-07-06 11:20:08 +0900

    Create new BACK TO HOME in enjoy activity

--- app/src/main/java/com/example/bt_tracker/Enjoy.java
+++ app/src/main/java/com/example/bt_tracker/Enjoy.java
@@ -2,7 +2,9 @@ package com.example.bt_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Enjoy extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class Enjoy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enjoy);
     }
+    public void backToHome(View view){
+        Intent backHome = new Intent(this,MainActivity.class);
+        startActivity(backHome);
+    }
 }
+
